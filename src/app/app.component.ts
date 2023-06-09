6c8673e47e047a92b01ab139994323c4d4129440
David Huillca <david@MacBook-Air-de-David.local>
2023-06-09 12:32:03 -0500

    Cambios de geolocation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'geolocation';
+
+  latitude: number = 0;
+  longitude: number = 0;
+
+  permissionDenied: boolean = false;
+  showPermissionMessage: boolean = false;
+
+  browser: string = '';
+
+  deviceInfo: any = null;
+
+  isMobile = false;
+  isTablet = false;
+  isDesktopDevice = false;
+
+  constructor(private deviceService: DeviceDetectorService) {
+    this.epicFunction();
+  }
+
+  epicFunction() {
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+    this.browser = this.deviceInfo.browser;
+
+    this.isMobile = this.deviceService.isMobile();
+    this.isTablet = this.deviceService.isTablet();
+    this.isDesktopDevice = this.deviceService.isDesktop();
+    console.log(this.deviceInfo);
+    console.log(`Is Mobile: ${this.isMobile}`);
+    console.log(`Is Tablet: ${this.isTablet}`);
+    console.log(`Is Desktop: ${this.isDesktopDevice}`);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.latitude = position.coords.latitude;
+          this.longitude = position.coords.longitude;
+          console.log("Ubicación obtenida con éxito.");
+        },
+        (error) => {
+          if (error.code === error.PERMISSION_DENIED) {
+            this.permissionDenied = true;
+            this.showPermissionMessage = true;
+          }
+          console.error("Error al obtener la ubicación:", error);
+        }
+      );
+    } else {
+      console.error("Geolocalización no es compatible con este navegador.");
+    }
+  }
 }
